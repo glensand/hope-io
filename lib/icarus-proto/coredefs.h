8f8d68c9c5245d8da1f92a3037ddebd926c64987
Gleb Bezborodov <bezborodoff.gleb@gmail.com>
2024-02-27 04:03:14 +0700

    missed changes

---
+++ lib/icarus-proto/coredefs.h
@@ -0,0 +1,15 @@
+/* Copyright (C) 2024 Gleb Bezborodov - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * terms of the MIT license.
+ *
+ * You should have received a copy of the MIT license with
+ * this file. If not, please write to: bezborodoff.gleb@gmail.com, or visit : https://github.com/glensand/daedalus-proto-lib
+ */
+
+#pragma once
+
+#if defined(_WIN32) || defined(_WIN64)
+#define ICARUS_WIN
+#else
+#define ICARUS_NIX
+#endif
